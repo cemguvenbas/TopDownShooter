eda76650f9714610b73ff9f7c331b8612b77bda9
Cem Güvenbaş <cemguvenbass@gmail.com>
2023-08-26 22:37:04 +0300

    Adding accuracy and recoil to our weapon

---
+++ GFA-TopDownShooter/Assets/Scripts/GFA/TPS/WeaponSystem/Weapon.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFA.TPS.WeaponSystem
+{
+    [CreateAssetMenu(menuName = "Weapon")]
+    public class Weapon : ScriptableObject
+    {
+        
+    }
+}
+
