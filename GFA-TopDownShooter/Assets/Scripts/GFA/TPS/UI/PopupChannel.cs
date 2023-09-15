684f67a17eb6ad7b5e83a3c132316b162dc94111
Cem Güvenbaş <cemguvenbass@gmail.com>
2023-09-15 17:21:09 +0300

    UI Popup

---
+++ GFA-TopDownShooter/Assets/Scripts/GFA/TPS/UI/PopupChannel.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GFA.TPS.UI
+{
+    public static class PopupChannel
+    {
+        //private List<Popup> _popups = new List<Popup>();
+        //public static Popup GetPopup()
+        //{
+                
+        //}
+    }
+}
+
